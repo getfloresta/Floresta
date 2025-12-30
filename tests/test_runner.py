12f735d84f72d94453025857bb3748f5f58e414e
moisesPomilio <93723302+moisesPompilio@users.noreply.github.com>
2025-12-30 18:40:05 -0300

    chore(tests): consolidate addnode_v1 and addnode_v2 into pytest framework
    - Merged the addnode_v1 and addnode_v2 tests into
    a single pytest file `addnode.py`.
    - Introduced the `TestAddNode` class to encapsulate
    the tests.
    - Added `test_add_node_v1` and `test_add_node_v2`
    methods to handle v1 and v2 transport protocol
    tests, respectively.

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -44,8 +44,6 @@ ALLDONE_EMOJI = "🎉"
 # Bitcoin Core's functional tests:
 # https://github.com/bitcoin/bitcoin/blob/master/test/functional/test_runner.py#L89
 BASE_TEST_SUITE = [
-    ("floresta-cli", "addnode-v2"),
-    ("floresta-cli", "addnode-v1"),
     ("florestad", "reorg-chain"),
     ("floresta-cli", "getblockcount"),
     ("floresta-cli", "uptime"),
