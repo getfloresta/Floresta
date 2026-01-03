86f808bd122d325fdc90e461d5012a10d84633db
moisesPomilio <93723302+moisesPompilio@users.noreply.github.com>
2026-01-02 21:31:26 -0300

    chore(functional): convert test getbestblockhash to pytest

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -45,7 +45,6 @@ ALLDONE_EMOJI = "🎉"
 # https://github.com/bitcoin/bitcoin/blob/master/test/functional/test_runner.py#L89
 BASE_TEST_SUITE = [
     ("floresta-cli", "getblockcount"),
-    ("floresta-cli", "getbestblockhash"),
     ("floresta-cli", "getblockhash"),
     ("floresta-cli", "gettxout"),
     ("floresta-cli", "ping"),
