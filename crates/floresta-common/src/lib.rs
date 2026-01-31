d2fb0d3a077e4ce857a84aedb608576e382dc7ff
moisesPomilio <93723302+moisesPompilio@users.noreply.github.com>
2026-01-31 01:49:47 -0300

    chore(wallet): centralize descriptor parsing logic in `floresta-watch-only` module
     Improved code organization by consolidating descriptor handling in a single module
    
    - Moved `parse_descriptors`, `parse_xpubs`, and `slip132` to the
    `floresta-watch-only` module.
    - Centralized descriptor and XPUB parsing logic, previously scattered
    across `floresta-node` and `floresta-common`.
    - Removed descriptor-related features and the `miniscript` dependency
    from `floresta-common`.

--- crates/floresta-common/src/lib.rs
+++ crates/floresta-common/src/lib.rs
@@ -18,10 +18,6 @@ use bitcoin::hashes::sha256;
 use bitcoin::hashes::Hash;
 use bitcoin::ScriptBuf;
 use bitcoin::VarInt;
-#[cfg(any(feature = "descriptors-std", feature = "descriptors-no-std"))]
-use miniscript::Descriptor;
-#[cfg(any(feature = "descriptors-std", feature = "descriptors-no-std"))]
-use miniscript::DescriptorPublicKey;
 use sha2::Digest;
 
 #[cfg(feature = "std")]
@@ -33,8 +29,6 @@ pub mod spsc;
 
 #[cfg(feature = "std")]
 pub use ema::Ema;
-#[cfg(any(feature = "descriptors-std", feature = "descriptors-no-std"))]
-use prelude::*;
 pub use spsc::Channel;
 
 /// Computes the SHA-256 digest of the byte slice data and returns a [Hash] from `bitcoin_hashes`.
@@ -84,26 +78,6 @@ pub mod service_flags {
 
     /// This peer supports UTREEXO filter messages
     pub const UTREEXO_FILTER: u64 = 1 << 25;
-}
-
-#[cfg(any(feature = "descriptors-std", feature = "descriptors-no-std"))]
-/// Takes an array of descriptors as `String`, performs sanity checks on each one
-/// and returns list of parsed descriptors.
-pub fn parse_descriptors(
-    descriptors: &[String],
-) -> Result<Vec<Descriptor<DescriptorPublicKey>>, miniscript::Error> {
-    let descriptors = descriptors
-        .iter()
-        .map(|descriptor| {
-            let descriptor = Descriptor::<DescriptorPublicKey>::from_str(descriptor.as_str())?;
-            descriptor.sanity_check()?;
-            descriptor.into_single_descriptors()
-        })
-        .collect::<Result<Vec<Vec<_>>, _>>()?
-        .into_iter()
-        .flatten()
-        .collect::<Vec<_>>();
-    Ok(descriptors)
 }
 
 #[cfg(not(feature = "std"))]
