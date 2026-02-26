37f74d80dc650cc72043a48a7984e99e8b7b5934
jaoleal <jgleal@protonmail.com>
2026-02-26 16:11:58 -0300

    chore: satisfy new lints
    As by frebruary 26, new lints were introduced, breaking CI.
    This commit satify them.

--- crates/floresta-wire/src/p2p_wire/node/chain_selector_ctx.rs
+++ crates/floresta-wire/src/p2p_wire/node/chain_selector_ctx.rs
@@ -1011,16 +1011,14 @@ where
                 self.handle_disconnection(peer, idx)?;
             }
 
-            PeerMessages::Block(block) => {
+            PeerMessages::Block(block)
                 // During chain selection we don't ask for blocks, unless it's an explicit
                 // user request made through the node handle. If it isn't, we punish this
                 // peer for sending an unrequested block.
-                if self.check_is_user_block_and_reply(block)?.is_some() {
+                if self.check_is_user_block_and_reply(block.clone())?.is_some() => {
                     error!("peer {peer} sent us a block we didn't request");
                     self.increase_banscore(peer, 5)?;
                 }
-            }
-
             _ => {}
         }
         Ok(())
