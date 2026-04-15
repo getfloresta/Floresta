a647e3d4cac6fae41216d471eb9eba4dc1c19892
Shallom Micah Bawa <micahshallom@gmail.com>
2026-04-16 00:34:37 +0100

    test(rpc): add assertions for new getblockchaininfo fields

--- crates/floresta-rpc/src/lib.rs
+++ crates/floresta-rpc/src/lib.rs
@@ -159,18 +159,7 @@ mod tests {
 
         assert_eq!(gbi.blocks, 0);
         assert_eq!(gbi.chain, "regtest".to_owned());
-        assert!(gbi.initialblockdownload);
-        assert_eq!(gbi.leaf_count, 0);
-        assert_eq!(gbi.root_hashes, Vec::<String>::new());
-    }
-
-    #[test]
-    fn test_get_roots() {
-        let (_proc, client) = start_florestad();
-
-        let gbi = client.get_blockchain_info().expect("rpc not working");
-
-        assert_eq!(gbi.root_hashes, Vec::<String>::new());
+        assert!(gbi.initial_block_download);
     }
 
     #[test]
